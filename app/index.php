f6875a83feb89925d691b22f0c66d3b8c758601d
Julio_Lozano <juliocamilolozanoguevara@gmail.com>
2026-05-13 15:19:36 -0500

    da

--- app/index.php
+++ app/index.php
@@ -6,7 +6,13 @@ define('BASE_PATH', dirname(__DIR__));
 require_once BASE_PATH . '/app/core/Env.php';
 require_once BASE_PATH . '/app/core/Config.php';
 
-Env::loadArray(require BASE_PATH . '/app/config/env.php');
+$envFile = BASE_PATH . '/.env';
+if (is_file($envFile)) {
+    Env::load($envFile);
+} else {
+    Env::loadArray(require BASE_PATH . '/app/config/env.php');
+}
+
 Config::set(require BASE_PATH . '/app/config/config.php');
 
 require_once BASE_PATH . '/app/core/helpers.php';
